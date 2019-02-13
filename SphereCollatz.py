216b32701d7bef421c1fb2fca9a23772830b0185
= <kevin.li128@gmail.com>
2019-02-12 20:01:52 -0600

    implement cache optimization 1

---
+++ SphereCollatz.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2016
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+import sys
+
+def collatz_read(s):
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# collatz_eval
+# ------------
+
+
+def collatz_eval(i, j):
+    
+    if (j >= i):
+        start = i
+        stop = j + 1
+    else:
+        start = j
+        stop = i + 1
+
+    count = [1]
+
+    for i in range (start, stop, 1):
+        tempCount = 1
+
+        while i != 1:
+            if ( i % 2 ) == 1:
+                i = (3*i) + 1
+                tempCount += 1
+            else:
+                i = i/2
+                tempCount += 1
+
+        count.append(tempCount)
+
+
+
+    return max(count)
+
+# -------------
+# collatz_print
+# -------------
+
+
+def collatz_print(w, i, j, v):
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+
+def collatz_solve(r, w):
+    """
+    r a reader
+    w a writer
+    """
+    for s in r:
+        i, j = collatz_read(s)
+        v = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+if __name__ == "__main__":
+    collatz_solve(sys.stdin, sys.stdout)
